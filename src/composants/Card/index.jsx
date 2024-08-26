c0ea204641f8ca2e1920f544e8cd652cb5b7855e
Valentin Belloncle <valentin.belloncle@hotmail.fr>
2024-08-26 19:41:18 +0200

    Version finale

--- src/composants/Card/index.jsx
+++ src/composants/Card/index.jsx
@@ -1,22 +1,15 @@
-import { Link } from "react-router-dom"
-import logements from "../../datas/logements.json"
-import styles from "./index.module.scss"
+import { Link } from "react-router-dom";
+import styles from "./index.module.scss";
 
-function Card() {
-  return (
-    <section>
-      <ul className={styles.gallery}>
-        {logements.map(({ id, cover, title, description }) =>
-          <Link key={id} to={`/property/${id}`}>
-            <li className={styles.gallery__card} key={id}>
-              <img src={cover} alt={description} />
-              <p>{title}</p>
-            </li>
-          </Link>
-        )}
-      </ul>
-    </section>
-  )
+function Card({ id, cover, title, description }) {
+	return (
+		<Link to={`/property/${id}`} className={styles.card}>
+			<article className={styles.gallery__card} key={id}>
+				<img src={cover} alt={description} />
+				<p>{title}</p>
+			</article>
+		</Link>
+	);
 }
 
-export default Card
+export default Card;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,30 @@
-import Banner from "../../composants/Banner"
-import Card from "../../composants/Card"
-import styles from "./index.module.scss"
+import Banner from "../../composants/Banner";
+import Card from "../../composants/Card";
+import styles from "./index.module.scss";
+import logements from "../../datas/logements.json";
 
 function Home() {
-  return (
-    <main className={styles.home}>
-      <Banner image="landscape1" content="Chez vous, partout et ailleurs"/>
-      <Card />
-    </main>
-    
-  )
+	return (
+		<main className={styles.home}>
+			<Banner
+				image="landscape1"
+				content="Chez vous, partout et ailleurs"
+			/>
+			<section>
+				<div className={styles.home__gallery}>
+					{logements.map(({ id, cover, title, description }) => (
+						<Card
+							key={id}
+							id={id}
+							cover={cover}
+							title={title}
+							description={description}
+						/>
+					))}
+				</div>
+			</section>
+		</main>
+	);
 }
-  
-export default Home
+
+export default Home;
